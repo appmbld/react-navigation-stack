1eeb2ad2acd6b25f6c17e67b854b4544d30a24ad
Hoang Pham <hopha@microsoft.com>
2019-05-23 10:54:42 -0700

    Add if user want to track velocity.

--- src/views/StackView/StackViewLayout.js
+++ src/views/StackView/StackViewLayout.js
@@ -376,6 +376,12 @@ class StackViewLayout extends React.Component {
       : Platform.OS === 'ios';
   }
 
+  _isTrackingVelocity() {
+    const trackVelocityEnabled = this.props.transitionProps.scene.descriptor
+      .options.trackGestureVelocityEnabled;
+    return typeof trackVelocityEnabled === 'boolean' ? trackVelocityEnabled : false;
+  }
+
   _isMotionVertical() {
     return this._isModal();
   }
@@ -584,7 +590,8 @@ class StackViewLayout extends React.Component {
     this.positionSwitch.setValue(1);
 
     const shouldGoBackCriterion =
-      gestureVelocity > 50 || value <= index - POSITION_THRESHOLD;
+      (this._isTrackingVelocity() && gestureVelocity > 50) ||
+      value <= index - POSITION_THRESHOLD;
 
     if (shouldGoBackCriterion && this.props.onActionBeforeSwipeBack) {
       this.props.onActionBeforeSwipeBack({
@@ -634,8 +641,6 @@ class StackViewLayout extends React.Component {
       transitionProps: { navigation, position, layout },
     } = this.props;
     const { index } = navigation.state;
-    const immediateIndex =
-      this._immediateIndex == null ? index : this._immediateIndex;
 
     // Calculate animate duration according to gesture speed and moved distance
     const distance = layout.height.__getValue();
@@ -658,25 +663,33 @@ class StackViewLayout extends React.Component {
 
     // If the speed of the gesture release is significant, use that as the indication
     // of intent
-    if (gestureVelocity < -50) {
-      this.props.onGestureCanceled && this.props.onGestureCanceled();
-      this._reset(immediateIndex, resetDuration);
-      return;
-    }
-    if (gestureVelocity > 50) {
-      this.props.onGestureEnd && this.props.onGestureEnd();
-      this._goBack(immediateIndex, goBackDuration);
-      return;
-    }
+    const shouldGoBackCriterion =
+      (this._isTrackingVelocity() && gestureVelocity > 50) ||
+      value <= index - POSITION_THRESHOLD;
 
-    // Then filter based on the distance the screen was moved. Over a third of the way swiped,
-    // and the back will happen.
-    if (value <= index - POSITION_THRESHOLD) {
-      this.props.onGestureEnd && this.props.onGestureEnd();
-      this._goBack(immediateIndex, goBackDuration);
+    if (shouldGoBackCriterion && this.props.onActionBeforeSwipeBack) {
+      this.props.onActionBeforeSwipeBack({
+        onContinue: () =>
+          this._navigateWithGesture(
+            this.props.onGestureEnd,
+            goBackDuration,
+            true
+          ),
+        onCancel: () =>
+          this._navigateWithGesture(
+            this.props.onGestureCanceled,
+            resetDuration,
+            false
+          ),
+      });
+    } else if (shouldGoBackCriterion) {
+      this._navigateWithGesture(this.props.onGestureEnd, goBackDuration, true);
     } else {
-      this.props.onGestureCanceled && this.props.onGestureCanceled();
-      this._reset(immediateIndex, resetDuration);
+      this._navigateWithGesture(
+        this.props.onGestureCanceled,
+        resetDuration,
+        false
+      );
     }
   }
 
